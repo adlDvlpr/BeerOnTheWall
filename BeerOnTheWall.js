520622ad8b8461eb1b72323a6fcb8dc51294eb20
adlDvlpr <99961161+adlDvlpr@users.noreply.github.com>
2022-02-26 05:51:42 +0100

    Create BeerOnTheWall.js

---
+++ BeerOnTheWall.js
@@ -0,0 +1,20 @@
+
+function beerOnTheWall(){
+
+    for (var beer = 15; beer > 0; beer--) {
+        
+        var beerMinus1 = beer - 1;
+
+        if (beer >= 3 ){
+
+        alert( beer + " bottles of beer on the wall, " + beer + " bottles of beer. " + " Take 1 down, " + beerMinus1 + " bottles of beer on the wall." );
+    }
+    }
+     if (beer = 2 ){
+        alert( beer + " bottles of beer on the wall, " + beer + " bottles of beer. " + " Take 1 down, " + "1" + " bottle of beer on the wall." );
+    }
+
+    if (beer = 1){
+        alert( beer + " bottle of beer on the wall, " + beer + " bottle of beer. " + " Take 1 down, " + beerMinus1 + " bottles of beer on the wall." );
+}
+}
